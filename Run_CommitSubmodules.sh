aeff71827475ec13b99e27b490f7e071da943288
owoDra <85442566+owoDra@users.noreply.github.com>
2023-12-30 16:27:45 +0900

    Test2

--- Run_CommitSubmodules.sh
+++ Run_CommitSubmodules.sh
@@ -6,8 +6,8 @@ fi
 COMMIT_MESSAGE="$1"
 git submodule foreach "echo 'Adding all files.'; git add ."
 git submodule foreach "echo 'Stashing.'; git stash"
-git submodule foreach "echo 'Checking out master branch.'; git checkout master"
-git submodule foreach "echo 'Applying stash.'; git stash apply"
+git submodule foreach "echo 'Checking out master branch.'; git checkout master -q"
+git submodule foreach "echo 'Applying stash.'; git stash apply -q"
 git submodule foreach "echo 'Readding all files.'; git add ."
 git submodule foreach "echo 'Committing changes.'; git commit -a -q -m '${COMMIT_MESSAGE}' || :"
 echo 'Adding main repo changes.'
