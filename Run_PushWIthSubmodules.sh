23210803aa61908c9bd8af45e694192c033ad2a5
owoDra <85442566+owoDra@users.noreply.github.com>
2023-12-30 19:33:32 +0900

    pluginコンフィグを更新

--- Run_PushWIthSubmodules.sh
+++ Run_PushWIthSubmodules.sh
@@ -1,2 +1,4 @@
-git push --recurse-submodules=on-demand origin main:main
-git config credential.helper store
+#git push --recurse-submodules=on-demand origin main:main
+#git config credential.helper store
+git submodule foreach "git push origin main:main"
+git push origin main:main
