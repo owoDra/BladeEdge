38c17d3426654f4f46511bac0b83a1a600b99442
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-09 19:13:44 +0900

    Update copyrights

--- Source/ProjectBE/Character/BESpectatorPawn.cpp
+++ Source/ProjectBE/Character/BESpectatorPawn.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2023 owoDra
+﻿// Copyright (C) 2024 owoDra
 
 #include "BESpectatorPawn.h"
 
--- Source/ProjectBE/ProjectBE.Build.cs
+++ Source/ProjectBE/ProjectBE.Build.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2023 owoDra
+﻿// Copyright (C) 2024 owoDra
 
 using UnrealBuildTool;
 
