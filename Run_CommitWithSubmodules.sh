2820bbd68402530e5c04012d88a6c7bab61d133b
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-09 19:38:45 +0900

    Add License

--- Run_CommitWithSubmodules.sh
+++ Run_CommitWithSubmodules.sh
@@ -14,7 +14,7 @@ fi
 git submodule foreach "echo 'Adding all files.'; git add ."
 git submodule foreach "echo 'Stashing.'; git stash"
 git submodule foreach "echo 'Checking out master branch.'; git checkout main || :"
-git submodule foreach "echo 'Applying stash.'; git stash apply --index || :"
+git submodule foreach "echo 'Applying stash.'; git stash pop --index || :"
 git submodule foreach "echo 'Readding all files.'; git add ."
 git submodule foreach "echo 'Committing changes.'; git commit -a -q -m '${COMMIT_MESSAGE}' || :"
 echo 'Adding main repo changes.'
